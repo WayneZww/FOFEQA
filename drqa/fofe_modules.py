ADD: Train Eval; MOD: Sigmoid to Softmax; FIX: batchsize>1 issue with padding mask; FIX: other minor bug fix.

drqa/fofe_modules.py
@@ -141,7 +141,7 @@ class fofe_res_block(nn.Module):
         return out
 
 
-#--------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------<<
 class fofe_linear_tricontext(nn.Module):
     def __init__(self, embedding_dim,  alpha, cand_len_limit=10, doc_len_limit=46, has_lr_ctx_cand_incl=True, has_lr_ctx_cand_excl=True):
         super(fofe_linear_tricontext, self).__init__()
@@ -242,10 +242,7 @@ class fofe_linear_tricontext(nn.Module):
         
         return context_alpha_buffers, cands_pos
     
-    def forward(self, x_input):
-        # TODO @SED [PRIORITY 1]: FIX PADDING / BATCHSIZE>1 ISSUE in DOC_FOFE
-        #                   IDEA: bmm(context_alpha_buffer, doc_mask) then sum each row;
-        #                         this should tell the row of the cands that included padding
+    def forward(self, x_input, x_mask):
         batch_size = x_input.size(0)
         length = min(x_input.size(1), self.doc_len_limit)
         embedding_dim = x_input.size(2)
@@ -265,7 +262,7 @@ class fofe_linear_tricontext(nn.Module):
         for i in range(n_context_types):
             _batchwise_alpha_buffer.append(context_alpha_buffer[i].expand(batch_size, n_cand, length))
             _batchwise_fofe_codes.append(torch.bmm(_batchwise_alpha_buffer[i],x_input))
-        
+
         if ( not self.has_lr_ctx_cand_incl ) and ( not self.has_lr_ctx_cand_excl ):
             batchwise_fofe_codes = _batchwise_fofe_codes[0]
         elif ( not self.has_lr_ctx_cand_incl ) and ( self.has_lr_ctx_cand_excl ):
@@ -283,10 +280,11 @@ class fofe_linear_tricontext(nn.Module):
                                               _batchwise_fofe_codes[4],
                                               _batchwise_fofe_codes[3]], dim=-1)
         batchwise_cands_pos = cands_pos.unsqueeze(0).expand(batch_size,n_cand, cands_pos.size(-1))
-        return batchwise_fofe_codes, batchwise_cands_pos
+        batchwise_padded_cands = torch.bmm(_batchwise_alpha_buffer[0], x_mask.float().unsqueeze(-1)) > 0
+    
+        return batchwise_fofe_codes, batchwise_cands_pos, batchwise_padded_cands
 
-#--------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------->>
 
 class fofe_linear(nn.Module):
     def __init__(self, channels, alpha): 
@@ -308,18 +306,27 @@ class fofe_linear(nn.Module):
         output = self.linear(fofe_code)
         return output
 
-
+#--------------------------------------------------------------------------------<<
 class fofe(nn.Module):
     def __init__(self, channels, alpha): 
         super(fofe, self).__init__()
         self.alpha = alpha
         
-    def forward(self, x):
+    def forward(self, x, x_mask):
         length = x.size(-2)
+        
+        # Construct Alphas Buffer (i.e. Matrix)
         matrix = x.new_empty(x.size(0),1,length)
         matrix[:,].copy_(torch.pow(self.alpha,torch.linspace(length-1,0,length)))
+        
+        # Adjust Alphas Buffer to accommondate the padding
+        # NOTED: alpha for the padding will become > 1, but noted that their corresponding x values are 0.
+        matrix_padding_rm = torch.pow(self.alpha,-1*torch.sum(x_mask,dim=1).float()).unsqueeze(-1).unsqueeze(-1)
+        matrix.copy_(torch.bmm(matrix_padding_rm, matrix))
+    
         fofe_code = torch.bmm(matrix,x).squeeze(-2)
         return fofe_code
+#-------------------------------------------------------------------------------->>
 
 
 class Simility(nn.Module):

drqa/fofe_reader.py
@@ -62,21 +62,30 @@ class FOFEReader(nn.Module):
         self.fofe_linear = fofe(opt['embedding_dim'], opt['fofe_alpha'])
         """
         self.fnn = nn.Sequential(
-            nn.Linear(doc_input_size*3+opt['embedding_dim'], opt['hidden_size']*4, bias=False),
+            nn.Linear(doc_input_size*n_ctx_types+opt['embedding_dim'], opt['hidden_size']*4, bias=False),
             nn.ReLU(inplace=True),
             nn.Linear(opt['hidden_size']*4, opt['hidden_size']*2, bias=False),
             nn.ReLU(inplace=True),
             nn.Linear(opt['hidden_size']*2, 1, bias=False),
             nn.Sigmoid()
         )
-        """
         self.fnn = nn.Sequential(
-            nn.Conv1d(doc_input_size*3+opt['embedding_dim'], opt['hidden_size']*4, 1, 1, bias=False),
+            nn.Conv1d(doc_input_size*n_ctx_types+opt['embedding_dim'], opt['hidden_size']*4, 1, 1, bias=False),
             nn.ReLU(inplace=True),
             nn.Conv1d(opt['hidden_size']*4, opt['hidden_size']*2, 1, 1, bias=False),
             nn.ReLU(inplace=True),
             nn.Conv1d(opt['hidden_size']*2, 1, 1, 1, bias=False),
             nn.Sigmoid()
+        )
+        """
+        self.fnn = nn.Sequential(
+            nn.Conv1d(doc_input_size*n_ctx_types+opt['embedding_dim'], opt['hidden_size']*4, 1, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(opt['hidden_size']*4, opt['hidden_size']*4, 1, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(opt['hidden_size']*4, opt['hidden_size']*2, 1, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(opt['hidden_size']*2, 2, 1, 1, bias=False)
         )
         self.count=0
     
@@ -113,11 +122,10 @@ class FOFEReader(nn.Module):
 
 
 
-    def sample_via_fofe_tricontext(self, doc_emb, query_emb, target_s=None, target_e=None):
-        # TODO @SED [PRIORITY 1]: FIX PADDING / BATCHSIZE>1 ISSUE in DOC_FOFE.
-        # TODO @SED [PRIORITY 2]: FIX PADDING / BATCHSIZE>1 ISSUE in QUERY_FOFE.
-        _doc_fofe, _cands_ans_pos = self.fofe_tricontext_encoder(doc_emb)
-        _query_fofe = self.fofe_linear(query_emb)
+    def sample_via_fofe_tricontext(self, doc_emb, query_emb, doc_mask, query_mask, target_s=None, target_e=None):
+        #TODO @SED: REMOVE REDUNDANT COPYING.
+        _query_fofe = self.fofe_linear(query_emb, query_mask)
+        _doc_fofe, _cands_ans_pos, _padded_cands_mask = self.fofe_tricontext_encoder(doc_emb, doc_mask)
 
         batch_size = _query_fofe.size(0)
         query_embedding_dim = _query_fofe.size(-1)
@@ -186,12 +194,21 @@ class FOFEReader(nn.Module):
             samples_target_score = target_score.new_empty(batch_size * sample_num, 1)
             samples_target_score.copy_(target_score.index_select(0, samples_idx))
 
-            #import pdb;pdb.set_trace()
+            # 4. Reshape samples_dq_input and samples_target_score to work on conv1d (instead of linear)
+            samples_dq_input = samples_dq_input.transpose(-1,-2).unsqueeze(0)
+            samples_target_score = samples_target_score.transpose(-1,-2).long()
+            
             return samples_dq_input, samples_target_score
         else:
             cands_ans_pos = _cands_ans_pos.new_empty(batch_size*n_cands_ans,_cands_ans_pos.size(-1))
             cands_ans_pos.copy_(_cands_ans_pos.contiguous().view([batch_size*n_cands_ans,_cands_ans_pos.size(-1)]))
-            return dq_input, cands_ans_pos
+
+            padded_cands_mask = _padded_cands_mask.contiguous().view([batch_size*n_cands_ans, 1])
+            
+            # 4. Reshape dq_input to work on conv1d (instead of linear)
+            dq_input = dq_input.transpose(-1,-2).unsqueeze(0)
+            
+            return dq_input, cands_ans_pos, padded_cands_mask
 
     #--------------------------------------------------------------------------------
 
@@ -352,15 +369,20 @@ class FOFEReader(nn.Module):
         if self.training :
             #--------------------------------------------------------------------------------            
             #dq_input, target_score = self.sample(doc_emb, query_emb, target_s, target_e)
-            dq_input, target_score = self.sample_via_fofe_tricontext(doc_emb, query_emb, target_s, target_e)
-            score = self.fnn(dq_input.transpose(-1,-2).unsqueeze(0))
-            loss = F.mse_loss(score, target_score.transpose(-1,-2).unsqueeze(0), size_average=False)
+            dq_input, target_score = self.sample_via_fofe_tricontext(doc_emb, query_emb, doc_mask, query_mask, target_s, target_e)
+            score = self.fnn(dq_input)
+            score = F.log_softmax(score, dim=1)
+            loss = F.nll_loss(score, target_score)
+            #import pdb;pdb.set_trace()
             return loss
         else :
             #--------------------------------------------------------------------------------
-            dq_input, cands_ans_pos  = self.sample_via_fofe_tricontext(doc_emb, query_emb)
-            score = self.fnn(dq_input.transpose(-1,-2).unsqueeze(0))
-            predict_s, predict_e = self.rank_tri_select(cands_ans_pos, score.squeeze(0).squeeze(0))
+            dq_input, cands_ans_pos, padded_cands_mask  = self.sample_via_fofe_tricontext(doc_emb, query_emb, doc_mask, query_mask)
+            score = self.fnn(dq_input)
+            score = F.softmax(score, dim=1)
+            score = score[:,1:,:].squeeze(0).squeeze(0)
+            score.data.masked_fill_(padded_cands_mask.squeeze(-1).data, -float('inf'))
+            predict_s, predict_e = self.rank_tri_select(cands_ans_pos, score)
             return predict_s, predict_e
             #--------------------------------------------------------------------------------
             

train_fofe.py
@@ -19,7 +19,7 @@ def main():
     args, log = setup()
     log.info('[Program starts. Loading data...]')
     # ---------------------------------------------------------------------------------
-    train, dev, dev_y, embedding, opt = load_data(vars(args))
+    train, train_y, dev, dev_y, embedding, opt = load_data(vars(args))
     # ---------------------------------------------------------------------------------
     log.info(opt)
     log.info('[Data loaded.]')
@@ -60,21 +60,34 @@ def main():
     for epoch in range(epoch_0, epoch_0 + args.epochs):
         log.warning('Epoch {}'.format(epoch))
         # train
-        if not args.test_only:
-            batches = BatchGen(train, batch_size=args.batch_size, gpu=args.cuda)
-            start = datetime.now()
-            for i, batch in enumerate(batches):
-                model.update(batch)
-                if i % args.log_per_updates == 0:
-                    log.info('> epoch [{0:2}] updates[{1:6}] train loss[{2:.5f}] remaining[{3}]'.format(
-                        epoch, model.updates, model.train_loss.value,
-                        str((datetime.now() - start) / (i + 1) * (len(batches) - i - 1)).split('.')[0]))
+        batches = BatchGen(train, batch_size=args.batch_size, gpu=args.cuda)
+        start = datetime.now()
+        for i, batch in enumerate(batches):
+            model.update(batch)
+            if i % args.log_per_updates == 0:
+                log.info('> epoch [{0:2}] updates[{1:6}] train loss[{2:.5f}] remaining[{3}]'.format(
+                    epoch, model.updates, model.train_loss.value,
+                    str((datetime.now() - start) / (i + 1) * (len(batches) - i - 1)).split('.')[0]))
+        log.debug('\n')
+        # ---------------------------------------------------------------------------------
+        # eval train set
+        batches_exclude_target = BatchGen(train, batch_size=args.batch_size, evaluation=True, gpu=args.cuda)
+        train_predictions = []
+        quarter_epochs = (epoch_0 + args.epochs) // 4
+        if epoch % quarter_epochs == 0:
+            for i, batch in enumerate(batches_exclude_target):
+                train_predictions.extend(model.predict(batch))
+                log.debug('> evaluating train set [{}/{}]'.format(i, len(batches_exclude_target)))
+            em, f1 = score(train_predictions, train_y)
+            log.warning("train EM: {} F1: {}".format(em, f1))
+            if em < 0.1:
+                log.warning('DEBUG: EM score below threshold, \
+                             hence outputing the prediction and target for debugging:\n\
+                             train_predictions:{}\ntrain_y:{}\n'.format(train_predictions, train_y))
             log.debug('\n')
-        # eval
-        if args.test_only and args.resume:
-            break 
-
-        batches = BatchGen(dev, batch_size=args.batch_size, test_train=args.test_train, evaluation=True, gpu=args.cuda)
+        # ---------------------------------------------------------------------------------
+        # eval dev set
+        batches = BatchGen(dev, batch_size=args.batch_size, evaluation=True, gpu=args.cuda)
         predictions = []
         for i, batch in enumerate(batches):
             predictions.extend(model.predict(batch))
@@ -91,9 +104,6 @@ def main():
                     model_file,
                     os.path.join(args.model_dir, 'best_model.pt'))
                 log.info('[new best model saved.]')
-
-        if args.test_only:
-            break
 
 
 def setup():
@@ -224,7 +234,7 @@ def lr_decay(optimizer, lr_decay):
 
 
 def load_data(opt):
-    with open(opt['meta_file'], 'rb') as f:
+    with open('./data/SQuAD/meta.msgpack', 'rb') as f:
         meta = msgpack.load(f, encoding='utf8')
     embedding = torch.Tensor(meta['embedding'])
     opt['pretrained_words'] = True
@@ -236,23 +246,23 @@ def load_data(opt):
     BatchGen.ner_size = opt['ner_size']
     with open(opt['data_file'], 'rb') as f:
         data = msgpack.load(f, encoding='utf8')
-    train = data['train']
-    if opt['test_train']:
-        data['train'].sort(key=lambda x: len(x[1]))
-        dev = [x[:] for x in data['train']]
-        dev_y = [[x[-3]] for x in data['train']]
-    else:
-        data['dev'].sort(key=lambda x: len(x[1]))
-        dev = [x[:-1] for x in data['dev']]
-        dev_y = [x[-1] for x in data['dev']]
-    return train, dev, dev_y, embedding, opt
+    # ---------------------------------------------------------------------------------
+    data['train'].sort(key=lambda x: len(x[1]))
+    train = [x[:-3]+x[-2:] for x in data['train']]
+    train_y = [[x[-3]] for x in data['train']]
+    data['dev'].sort(key=lambda x: len(x[1]))
+    dev = [x[:-1] for x in data['dev']]
+    dev_y = [x[-1] for x in data['dev']]
+    return train, train_y, dev, dev_y, embedding, opt
+    # ---------------------------------------------------------------------------------
+
 
 
 class BatchGen:
     pos_size = None
     ner_size = None
 
-    def __init__(self, data, batch_size, gpu, test_train=False, evaluation=False):
+    def __init__(self, data, batch_size, gpu, evaluation=False):
         """
         input:
             data - list of lists
@@ -261,7 +271,6 @@ class BatchGen:
         self.batch_size = batch_size
         self.eval = evaluation
         self.gpu = gpu
-        self.test_train = test_train
 
         # sort by len
         data = sorted(data, key=lambda x: len(x[1]))
@@ -282,11 +291,15 @@ class BatchGen:
         for batch in self.data:
             batch_size = len(batch)
             batch = list(zip(*batch))
-
-            if self.eval and not self.test_train:
+            if self.eval:
+                # ---------------------------------------------------------------------------------
+                # if self.eval and len(batch) == 10, then this is a training set eval (so remove last 2 target cols)
+                if len(batch) == 10:
+                    batch = batch[:-2]
+                # ---------------------------------------------------------------------------------
                 assert len(batch) == 8
             else:
-                assert len(batch) == 11
+                assert len(batch) == 10
 
             context_len = max(len(x) for x in batch[1])
             context_id = torch.LongTensor(batch_size, context_len).fill_(0)
@@ -319,9 +332,9 @@ class BatchGen:
             question_mask = torch.eq(question_id, 0)
             text = list(batch[6])
             span = list(batch[7])
-            if not self.eval or self.test_train:
-                y_s = torch.LongTensor(batch[-2])
-                y_e = torch.LongTensor(batch[-1])
+            if not self.eval:
+                y_s = torch.LongTensor(batch[8])
+                y_e = torch.LongTensor(batch[9])
             if self.gpu:
                 context_id = context_id.pin_memory()
                 context_feature = context_feature.pin_memory()
@@ -330,7 +343,7 @@ class BatchGen:
                 context_mask = context_mask.pin_memory()
                 question_id = question_id.pin_memory()
                 question_mask = question_mask.pin_memory()
-            if self.eval and not self.test_train:
+            if self.eval:
                 yield (context_id, context_feature, context_tag, context_ent, context_mask,
                        question_id, question_mask, text, span)
             else:
@@ -397,4 +410,3 @@ def score(pred, truth):
 
 if __name__ == '__main__':
     main()
-