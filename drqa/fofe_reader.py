b3c60e76cbfdb1168854846d6613caa30ef24e35
Waynezww <waynebxz@gmail.com>
2018-07-21 16:57:03 -0400

    add dropout

--- drqa/fofe_reader.py
+++ drqa/fofe_reader.py
@@ -87,7 +87,7 @@ class FOFEReader(nn.Module):
 #                nn.Conv1d(opt['hidden_size']*4, opt['hidden_size']*4, 1, 1, bias=False),
 #                nn.BatchNorm1d( opt['hidden_size']*4),
 #                nn.ReLU(inplace=True),
-#                nn.Dropout(0.1),
+                nn.Dropout(0.1),
                 nn.Conv1d(opt['hidden_size']*4, 2, 1, 1, bias=False),
             )
         else:
